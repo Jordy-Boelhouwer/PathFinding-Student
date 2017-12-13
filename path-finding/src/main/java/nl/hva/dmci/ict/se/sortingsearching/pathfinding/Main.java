b62559ec0e2fc667aeac2238ff24ed6f3cb837bb
Yoeri <yoerimoens@gmail.com>
2017-12-14 00:04:22 +0100

    dijk klaar marshall moet nog

--- path-finding/src/main/java/nl/hva/dmci/ict/se/sortingsearching/pathfinding/Main.java
+++ path-finding/src/main/java/nl/hva/dmci/ict/se/sortingsearching/pathfinding/Main.java
@@ -1,10 +1,12 @@
 package nl.hva.dmci.ict.se.sortingsearching.pathfinding;
 
+import java.util.ArrayList;
 import nl.hva.dmci.ict.se.sortingsearching.weigthedgraph.DirectedEdge;
 import nl.hva.dmci.ict.se.sortingsearching.weigthedgraph.EdgeWeightedDigraph;
 
 /**
  * TODO make sure your code is compliant with the HBO-ICT coding conventions!
+ *
  * @author ???
  */
 public class Main {
@@ -14,31 +16,47 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO Here you can do your experiments.
-    	
-    	// Please have a good look at the constructors of EdgeWeightedDigraph!
-    	
-    	// Before you save any images make sure the value of TileWorldUtil.outputDir points to an
-    	// existing folder and ands with a '/'!
-    	// Example: TileWorldUtil.outputDir = "/Users/nico/output/";
-        
-        EdgeWeightedDigraph ewd = new EdgeWeightedDigraph("i5");
-        Dijkstra dijk = new Dijkstra(ewd, ewd.getStart());
-        ewd.show("i2", "test");
-        
-        if(dijk.hasPathTo(ewd.getEnd())){
-            Iterable<DirectedEdge> iterator = dijk.pathTo(ewd.getEnd());
-            ewd.tekenPad(iterator);
-            
-            System.out.println("Lengte van het pad: " + dijk.getTellerLengte());
-            System.out.println("Aantal knopen: " + dijk.getTellerKnopen());
-            
-        }else{
-            System.out.println("sukkel er is geen pad!!!111");
+
+        // Please have a good look at the constructors of EdgeWeightedDigraph!
+        // Before you save any images make sure the value of TileWorldUtil.outputDir points to an
+        // existing folder and ands with a '/'!
+        // Example: TileWorldUtil.outputDir = "/Users/nico/output/";
+        for (int i = 1; i < 22; i++) {
+            String img = "i" + Integer.toString(i);
+            System.out.println("=== WERELD: " + img + " ===");
+
+            EdgeWeightedDigraph ewd = new EdgeWeightedDigraph(img);
+            Dijkstra dijk = new Dijkstra(ewd, ewd.getStart());
+
+            if (dijk.hasPathTo(ewd.getEnd())) {
+                Iterable<DirectedEdge> iter = dijk.pathTo(ewd.getEnd());
+                ArrayList<Double> weight = new ArrayList();
+                iter.forEach((iterator) -> {
+                    weight.add(iterator.weight());
+                });
+
+                double totalWeight = 0;
+                int numNodes = 0;
+
+                for (double d : weight) {
+                    totalWeight += d;
+                    numNodes++;
+                }
+
+                System.out.println("Aantal knopen vergeleken: " + dijk.getTellerKnopen());
+                System.out.println("Lengte van het pad: " + dijk.getTellerLengte());
+                System.out.println("Kosten van de hele route: " + totalWeight);
+                System.out.println(iter);
+                ewd.tekenPad(iter);
+
+            } else {
+                System.out.println("Aantal knopen vergeleken: " + dijk.getTellerKnopen());
+                System.out.println("Er is geen pad mogelijk");
+            }
+
+            dijk.distTo(ewd.getEnd());
         }
-        
-        dijk.distTo(ewd.getEnd());
-       
-        
+
     }
-    
+
 }
